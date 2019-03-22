05204c22834914a2446a48aab4095a6f8bbec236
Filipe Pinheiro <filipe.alves.pinheiro@gmail.com>
2019-03-22 20:59:36 -0300

    Created login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+// Disable Materials animations
+// import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+// Active Materials animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Materials Modules
+import { MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
