5819ced39dee2f34582d13b0a9be47d51d664604
Filipe Pinheiro <filipe.alves.pinheiro@gmail.com>
2020-05-30 23:47:31 +0100

    Fix to build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.99.100:8089/reward-program/api/v1'
+  apiUrl: 'http://192.168.99.100:8089/reward-program/api/v1',
+  refreshTokenMillis: 10
 };
